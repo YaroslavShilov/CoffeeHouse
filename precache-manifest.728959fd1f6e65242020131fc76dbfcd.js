f848236eb0767d4b65d4d459e210613454bf69f8
Yaroslav Shilov <workyarik@gmail.com>
2020-06-24 21:53:33 +0300

    Updates

---
+++ precache-manifest.728959fd1f6e65242020131fc76dbfcd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6487a5216c80a629a0a0f95fd0584cd",
+    "url": "/CoffeeHouse/index.html"
+  },
+  {
+    "revision": "07ff5014924bb4c82066",
+    "url": "/CoffeeHouse/static/css/main.70c7756d.chunk.css"
+  },
+  {
+    "revision": "a18bb9bc0cb9fda21cc8",
+    "url": "/CoffeeHouse/static/js/2.31385e89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CoffeeHouse/static/js/2.31385e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ff5014924bb4c82066",
+    "url": "/CoffeeHouse/static/js/main.92dea95a.chunk.js"
+  },
+  {
+    "revision": "516ea5ecd14b6475ab30",
+    "url": "/CoffeeHouse/static/js/runtime-main.9f0bb6fa.js"
+  },
+  {
+    "revision": "831b2dd9a4dc12682e90cc01b1c0e480",
+    "url": "/CoffeeHouse/static/media/Merienda-Bold.831b2dd9.ttf"
+  },
+  {
+    "revision": "47a69df010ec81bd9bb2afa95605dbe1",
+    "url": "/CoffeeHouse/static/media/Merienda-Regular.47a69df0.ttf"
+  },
+  {
+    "revision": "c8b9f1eba50b41b951bde4eb8f0158a9",
+    "url": "/CoffeeHouse/static/media/aboutOurBeans.c8b9f1eb.jpg"
+  },
+  {
+    "revision": "b030a81de257fd075d0fc025dcadd990",
+    "url": "/CoffeeHouse/static/media/aboutOurGoods.b030a81d.jpg"
+  },
+  {
+    "revision": "59731da761217829697c620aa4f0a7af",
+    "url": "/CoffeeHouse/static/media/bg.59731da7.jpg"
+  },
+  {
+    "revision": "b69bd070042bf27627c518c04add96f2",
+    "url": "/CoffeeHouse/static/media/bg.b69bd070.jpg"
+  },
+  {
+    "revision": "d4775bf33e1c5aee68410a9df3b1c8a6",
+    "url": "/CoffeeHouse/static/media/bg.d4775bf3.jpg"
+  },
+  {
+    "revision": "fdf0f46aed5dda18de48307c09c2f0b2",
+    "url": "/CoffeeHouse/static/media/pleasurePage.fdf0f46a.jpg"
+  }
+]);
